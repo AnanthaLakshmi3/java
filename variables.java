7a4b335daa662a9ac02daf121859740fd116339b
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2025-04-01 20:38:05 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,17 @@
+
+public class variables {
+
+    //instance variable32
+    int age = 30;
+    //static variablr
+    static String name = "Anu";
+
+    public static void main(String[] args) {
+        String Queen = "Anantha";
+        System.out.println(name);
+        System.out.println(Queen);
+
+        variables persons = new variables();
+        System.out.println(persons.age);
+    }
+}
