7aee36b8957d806ba4fe5ec7754dd92d7c82855e
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2025-04-01 20:37:19 +0530

    Add files via upload

---
+++ data_types.java
@@ -0,0 +1,20 @@
+public class data_types{
+    public static void main(String[] args){
+     int num = 120;
+     long u = 89034L;
+     short k = 256;   
+     float n = 5.6f;
+     double d = 5.6;
+     boolean isAdamn = true;
+     char gender ='M';
+     String name = "ANU";
+     System.out.println(num);
+     System.out.println(n);
+     System.out.println(k);
+     System.out.println(u);
+     System.out.println(isAdamn);
+     System.out.println(d);
+     System.out.println(gender);
+     System.out.println(name);
+    }
+}
