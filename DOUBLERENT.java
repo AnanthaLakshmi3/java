08e58ea6348207dd5a3e34201663506d921c4e7f
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2025-05-06 21:07:55 +0530

    Add files via upload

---
+++ DOUBLERENT.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner myObj = new Scanner(System.in);
+		int x =myObj.nextInt();
+		System.out.println(x*2);
+
+	}
+}
