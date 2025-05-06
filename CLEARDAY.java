28ad7ae5f851f00011ebe79b4f68954ac7e7eabf
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2025-05-06 21:08:32 +0530

    Add files via upload

---
+++ CLEARDAY.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner myObj = new Scanner(System.in);
+		int x =myObj.nextInt();
+		int y=myObj.nextInt();
+		System.out.println(7-x-y);
+
+	}
+}
