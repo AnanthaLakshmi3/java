aa9608b2239ae3c53e15736a6647911709630dfe
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2025-05-07 20:21:06 +0530

    Add files via upload

---
+++ CHEFPAROLE.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner myObj = new Scanner(System.in);
+		int x = myObj.nextInt();
+		if (x>=7){
+		    System.out.println("Yes");
+		}
+		else{
+		    System.out.println("No");
+		}
+
+	}
+}
