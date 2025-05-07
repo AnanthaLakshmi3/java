9e335bb5119f69bcdc97a42ae184940f1b4167f4
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2025-05-07 20:16:44 +0530

    Add files via upload

---
+++ ELHP.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner myObj = new Scanner(System.in);
+		int x = myObj.nextInt();
+		int y = myObj.nextInt();
+		int k = 2*y ;
+		if (x>=k){
+		    System.out.println("YES");
+		}
+		else{
+		    System.out.println("No");
+		}
+
+	}
+}
