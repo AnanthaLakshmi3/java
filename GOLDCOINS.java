ed480fd0c46c64a919ababf991527037a75603a6
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2025-05-08 20:46:08 +0530

    Add files via upload

---
+++ GOLDCOINS.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner myObj = new Scanner(System.in);
+		int a = myObj.nextInt();
+		int b = myObj.nextInt();
+		int x = myObj.nextInt();
+		int y = myObj.nextInt();
+		if (x>y){
+		    System.out.println(a);
+		}
+		else{
+		    System.out.println(b);
+		}
+	}
+}
