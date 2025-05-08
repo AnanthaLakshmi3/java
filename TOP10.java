5711e54dd50d5e83a68cd3c3515402cb77f56b91
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2025-05-08 20:45:17 +0530

    Add files via upload

---
+++ TOP10.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner myObj = new Scanner(System.in);
+		int t = myObj.nextInt();
+		while (t-->0){
+		    int x = myObj. nextInt();
+		    if (x<=10){
+		        System.out.println("YES");
+		    }
+		    else{
+		        System.out.println("NO");
+		    }
+		}
+
+	}
+}
