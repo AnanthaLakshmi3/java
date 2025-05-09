001491a8a53caadacac43afe4157a1ced6256591
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2025-05-09 20:15:31 +0530

    Add files via upload

---
+++ NEWSPAPER.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner myObj = new Scanner(System.in);
+		int x = myObj.nextInt();
+		if (x==8 || x==9 || x==10){
+		    System.out.println("YES");
+		}
+		else{
+		    System.out.println("NO");
+		}
+
+	}
+}
