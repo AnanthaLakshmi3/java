bf8ef95e1c64edf4fb0f7ebad568f339900a9d71
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2025-05-10 21:06:25 +0530

    Add files via upload

---
+++ KITCHENTIME.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner myObj = new Scanner(System.in);
+		int t = myObj.nextInt();
+		while (t-->0){
+		  int x = myObj.nextInt();
+		  int y = myObj.nextInt();
+		  System.out.println(y-x);
+		}
+		
+
+	}
+}
