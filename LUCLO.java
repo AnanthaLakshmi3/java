fa825fb2155c0d00a1d30473c3d5a4963719b40f
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2025-05-16 19:41:23 +0530

    Add files via upload

---
+++ LUCLO.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner myObj = new Scanner(System.in);
+		int x =myObj.nextInt();
+		int k=((x-1)*3);
+		System.out.println(k+4);
+		
+		
+        myObj.close();
+	}
+}
