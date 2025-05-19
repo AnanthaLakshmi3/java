b20f133c6c0b2c2062d20ce03c3f42c2d459dea3
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2025-05-19 19:59:56 +0530

    Add files via upload

---
+++ PZSPLIT.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int v=sc.nextInt();
+		if(v%2==0){
+		    System.out.println("1");
+		}else{
+		    System.out.println("2");
+		}
+
+	}
+}
