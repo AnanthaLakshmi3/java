a21b644a4aaed28edfec025dcacb8ff58810c06f
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2025-05-22 20:00:59 +0530

    Add files via upload

---
+++ CLSI.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		int k = y*10;
+		if (k<=x){
+		    System.out.println("YES");
+		}
+		else{
+		    System.out.println("NO");
+		}
+
+	}
+}
